1eaf04cef31bf4981343b19380b4aed1174a18e9
Coursera Learner <nobody@nowhere.nul>
2019-08-26 00:17:14 +0000

    "recursive"

---
+++ 23_power_rec/power.c
@@ -0,0 +1,18 @@
+#include <stdio.h
+#include<stdlib>
+
+unsigned power(unsigned x, unsigned y){
+
+  unsigned ans = 1;
+  
+  if(y==0)
+    return 1;
+
+  if(y==1)
+    return x;
+  ans = x*power(x,y-1);
+
+  return ans;
+
+
+}
