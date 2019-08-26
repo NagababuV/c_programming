550c87f445c1c49d816e2d31178fe936ab1c6f0a
Coursera Learner <nobody@nowhere.nul>
2019-08-26 10:35:48 +0000

    ""

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -5,16 +5,16 @@
 
 int card_ptr_comp(const void * vp1, const void * vp2) {
   const card_t *const *cp1=vp1;
-  const card_t *const *cp2=cp2;
+  const card_t *const *cp2=vp2;
 
   if((**cp1).value > (**cp2).value)
     return -1;
   else if((**cp1).value < (**cp2).value)
     return 1;
   else{
-    if((**cp1).suit < (**cp2).suit)
+    if((**cp1).suit > (**cp2).suit)
       return -1;
-    else if((**cp1).suit > (**cp2).suit)
+    else if((**cp1).suit < (**cp2).suit)
       return 1;
   }
   
