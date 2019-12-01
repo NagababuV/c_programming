2a5cefc9eb3404920624b9d19fe6db5c409e2d8c
Coursera Learner <nobody@nowhere.nul>
2019-12-01 17:15:19 +0000

    c4

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -61,7 +61,7 @@ void assert_full_deck(deck_t * d) {
   }
 }
 
-/*
+
 void add_card_to(deck_t *deck, card_t c){
 
   deck->n_cards++;
@@ -144,4 +144,4 @@ void free_deck(deck_t *deck){
   free(deck);
 
 }
-*/
+
