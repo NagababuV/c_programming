a9893506b5b131871eaa7a631f6afd24b6f394fe
Coursera Learner <nobody@nowhere.nul>
2019-12-01 06:55:26 +0000

    "sorted"

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -19,6 +19,68 @@ void sortData(char ** data, size_t count) {
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
+  if(argc == 0){
+    fprintf(stderr,"few arguments\n");
+    return EXIT_FAILURE;
+  }
+
+  if(argc == 1){
+    char **arr=NULL;
+    char *line=NULL;
+    size_t sz=0;
+    int i =0;
+
+    while(getline(&line,&sz,stdin) >= 0){
+      arr = realloc(arr,(i+1)*sizeof(*arr));
+      arr[i] = line;
+      line = NULL;
+      i++;
+    }
+    free(line);
+    sortData(arr,i);
+
+    int j;
+    for(j=0; j<i;j++){
+      printf("%s",arr[j]);
+      free(arr[j]);
+    }
+    free(arr);
+  }
+  else{
+    int i;
+    for(i=0; i<argc;i++){
+      char **arr =NULL;
+      char *line =NULL;
+      size_t sz=0;
+      int j = 0;
+
+      FILE *f = fopen(argv[i],"r");
+      if(f==NULL){
+	perror("Could no open file");
+	return EXIT_FAILURE;
+      }
+      while(getline(&line,&sz,f) >= 0){
+	arr = realloc (arr,(j+1)*sizeof(*arr));
+	arr[j]=line;
+	line = NULL;
+	j++;
+      }
+      free(line);
+      sortData(arr,j);
+      int k;
+      for(k=0; k<j;k++){
+	printf("%s",arr[k]);
+	free(arr[k]);
+      }
+      free(arr);
+      if(fclose(f)!= 0){
+	perror("Failed to close input file");
+	return EXIT_FAILURE;
+      }
+      
+    }
+  
   
   return EXIT_SUCCESS;
+  }
 }
