62f6f8c1814473abebe03be4625677e51e03902c
Coursera Learner <nobody@nowhere.nul>
2019-12-01 06:21:36 +0000

    "idk"

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -5,4 +5,9 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char *out = malloc((strlen(inputName)+4)*sizeof(*out));
+  strcpy(out,inputName);
+  out = realloc(out,(strlen(inputName)+10)*sizeof(*out));
+  strcat(out,".counts");
+  return out;
 }
